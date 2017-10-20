d6607aa572c0b086fbefc12de1b181909fa7967b
compsci <compsci@compsci>
2017-10-20 10:43:25 -0600

    work in progress

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -1,16 +1,19 @@
 package caesar;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class BeginHere {
 
 	public static void main(String[] args) {
-		Scanner kbd = new Scanner(System.in);
 		CaesarCipher myCipher = new CaesarCipher();
+		String message = "";
 		
-		System.out.println("Please enter the plain text to encode");
-		String plainText = "";
-
+		message = "Please enter the plain text to encode:";
+		String plainText = JOptionPane.showInputDialog(message);
+		
+		String codedText = myCipher.encode(plainText);
+		JOptionPane.showMessageDialog(null, codedText);
+		
 	}
 
 }
--- src/caesar/CaesarCipher.java
+++ src/caesar/CaesarCipher.java
@@ -1,5 +1,40 @@
 package caesar;
 
+import javax.swing.JOptionPane;
+
 public class CaesarCipher {
+	
+	private String alpha = "abcdefghijklmnopqrstuvwyxz";
+
+	public String encode(String plainText) {
+		String message = "";
+		int encodeKey = 0;
+		int Key2 = 0;
+		
+		message = "please enter the key: ";
+		String key = JOptionPane.showInputDialog(message);
+		
+		message = "your key is: " + key;
+		JOptionPane.showMessageDialog(null, message);
+		Integer.parseInt(key);
+		
+		System.out.println("test, aplha length = " + alpha.length());
+		System.out.println("test, first char is " + alpha.charAt(0));
+		System.out.println("test, last char is " + alpha.charAt(alpha.length()-1));
+		
+		for(int i = 0; i < alpha.length(); i++) {
+			System.out.println(alpha.charAt(i));
+		}
+		
+		System.out.println("\n \n");
+		
+		for(int i = 0; i < plainText.length(); i++) {
+			encodeKey = i + Key2;
+			System.out.println(plainText.charAt(encodeKey));
+		}
+		
+		
+		return plainText;
+	}
 
 }
