0a9ff20415be4244a1702a5456f5c66afade717a
compsci <compsci@compsci>
2017-10-20 09:59:10 -0600

    initial commit

---
+++ src/caesar/BeginHere.java
@@ -0,0 +1,16 @@
+package caesar;
+
+import java.util.Scanner;
+
+public class BeginHere {
+
+	public static void main(String[] args) {
+		Scanner kbd = new Scanner(System.in);
+		CaesarCipher myCipher = new CaesarCipher();
+		
+		System.out.println("Please enter the plain text to encode");
+		String plainText = "";
+
+	}
+
+}
