ba27d058d5094aaedfca601028be83c626791fc8
compsci <compsci@compsci>
2017-10-26 09:59:39 -0600

    added graded asignment

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -3,14 +3,15 @@ package caesar;
 import java.util.Scanner;
 
 public class BeginHere {
-
+	
+	static Scanner kbd = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Scanner kbd = new Scanner(System.in);
-		
+
 		CaesarCipher myCipher = new CaesarCipher();
 		
 		System.out.println("Please enter the plain text to encode:");
-		String plainText = kbd.nextLine();
+		String plainText = kbd.nextLine().toLowerCase();
 		
 		System.out.println("Please enter the key:");
 		int key = kbd.nextInt();
@@ -18,7 +19,25 @@ public class BeginHere {
 		
 		String codedText = myCipher.encode(plainText, key);
 		System.out.println("Your secret message is: " + codedText);
+		
 
 	}
 
 }
+
+/*grade checkpoint 
+ * 
+ * 1.encoding
+ * 	JOP
+ * 	jar file
+ * 	do you want to encode decode or crack?
+ * 	ask plain text msg
+ * 	ask for key
+ * 	display encoded message
+ * 	ask if you want to do another msg
+ * 	graceful exit or go again
+ * 2.decoding
+ * 3.cracking 
+ * 
+ * 
+ */
